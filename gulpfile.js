388f1b7521805b67523ac672d7facdd37682b29f
Rubberkat <roydejager@live.nl>
2016-07-01 15:51:38 +0200

    Added navbar

--- gulpfile.js
+++ gulpfile.js
@@ -25,3 +25,19 @@ gulp.task('watch', function() {
    gulp.watch('src/scss/**/*.scss', ['sass']);
 });
 
+module.exports = function (app, nunjucks, dir) {
+
+    app.set('views', dir.views);
+    app.set('view engine', 'nunj');
+    app.set('view cache', app.get('env') === 'production');
+    nunjucks.configure('views', {
+        autoescape: true,
+        express: app
+    });
+
+    app.use(function (req, res, next) {
+        res.locals.user = req.session.user || false;
+        next();
+    });
+
+};
